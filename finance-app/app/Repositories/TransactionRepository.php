d89c63161c165f114bb2e7241763a5eeeb3addfe
S-Andor <91542001+S-Andor@users.noreply.github.com>
2022-12-25 17:12:21 +0200

    switched to vuetify and added transactions

--- finance-app/app/Repositories/TransactionRepository.php
+++ finance-app/app/Repositories/TransactionRepository.php
@@ -3,13 +3,30 @@
 namespace App\Repositories;
 
 use App\Interfaces\ITransactionRepository;
+use App\Models\Transaction;
 use App\Models\TransactionCategory;
+use DateTime;
+use DateTimeZone;
 
 class TransactionRepository implements ITransactionRepository
 {
 
     public function getCategories()
     {
-        return TransactionCategory::all(['id','name','code']);
+        return TransactionCategory::all(['id','name','code','type']);
+    }
+    public function saveTransaction($transaction)
+    {
+        $date = new DateTime('now',new DateTimeZone('UTC'));
+        return Transaction::query()->insert(
+            [
+                'transaction_category_id' => $transaction->get('category_id'),
+                'user_id' => $transaction->get('user_id'),
+                'date' => $transaction->get('date'),
+                'amount' => $transaction->get('amount'),
+                'created_at' => $date,
+                'updated_at' => $date,
+            ],
+        );
     }
 }
